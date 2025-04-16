8a97b5589c40a7e7537b093064663ca50ddde987
unknown <ya-mihim@yandex.ru>
2025-04-16 10:06:34 +0300

    TrackEntity

---
+++ app/src/main/java/com/example/playlist_maker2/player/data/db/entities/TrackEntity.kt
@@ -0,0 +1,29 @@
+package com.example.playlist_maker2.player.data.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.ColumnInfo.Companion.INTEGER
+import androidx.room.ColumnInfo.Companion.TEXT
+import androidx.room.PrimaryKey
+
+data class TrackEntity(
+    @PrimaryKey @ColumnInfo(name = "trackId", typeAffinity = TEXT)
+    val trackId:String,
+    @ColumnInfo(name = "trackName", typeAffinity = TEXT)
+    val trackName: String,
+    @ColumnInfo(name = "artistName", typeAffinity = TEXT)
+    val artistName: String,
+    @ColumnInfo(name = "trackTimeMillis", typeAffinity = INTEGER)
+    val trackTimeMillis: Int,
+    @ColumnInfo(name = "artworkUrl100", typeAffinity = TEXT)
+    val artworkUrl100: String,
+    @ColumnInfo(name = "collectionName", typeAffinity = TEXT)
+    val collectionName: String,
+    @ColumnInfo(name = "releaseDate", typeAffinity = TEXT)
+    val releaseDate: String,
+    @ColumnInfo(name = "primaryGenreName", typeAffinity = TEXT)
+    val primaryGenreName: String,
+    @ColumnInfo(name = "country", typeAffinity = TEXT)
+    val country: String,
+    @ColumnInfo(name = "previewUrl", typeAffinity = TEXT)
+    val previewUrl: String
+)
