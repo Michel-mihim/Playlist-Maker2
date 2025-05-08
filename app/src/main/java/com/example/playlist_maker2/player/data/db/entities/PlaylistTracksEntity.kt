f8dacb2f86eac6a25d1700c6ea98c06ab37fbb6d
Michel-mihim <ya-mihim@yandex.ru>
2025-05-08 20:57:21 +0300

    before_playerActivity_action

--- app/src/main/java/com/example/playlist_maker2/player/data/db/entities/PlaylistTracksEntity.kt
+++ app/src/main/java/com/example/playlist_maker2/player/data/db/entities/PlaylistTracksEntity.kt
@@ -2,11 +2,10 @@ package com.example.playlist_maker2.player.data.db.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "playlist_tracks_table")
+@Entity(tableName = "playlist_tracks_table", primaryKeys = ["playlistName", "trackId"])
 data class PlaylistTracksEntity(
-    @PrimaryKey @ColumnInfo(name = "playlistName", typeAffinity = ColumnInfo.TEXT)
+    @ColumnInfo(name = "playlistName", typeAffinity = ColumnInfo.TEXT)
     val playlistName: String,
     @ColumnInfo(name = "trackId", typeAffinity = ColumnInfo.TEXT)
     val trackId: String
